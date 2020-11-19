53ca8eb56a50dccab0d2805639fb35770a1a0e39
naimish bhagat <naimish.bhagat@arq.group>
2020-11-20 08:46:51 +1100

    Cocktails project

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about Us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima commodi
+        eius doloremque. Ullam deleniti iure enim nisi, corrupti modi nihil
+        magni veniam? Enim dolor officia, nulla excepturi, nostrum quibusdam
+        pariatur assumenda consectetur ea eum incidunt illo voluptate fugit.
+        Architecto ea tempora ipsa laudantium nobis pariatur libero ipsam harum
+        beatae soluta. Possimus ut error natus. Quam necessitatibus nihil nobis,
+        itaque sunt sint qui temporibus. Corrupti obcaecati neque incidunt
+        delectus, minima dolor cumque accusamus rerum iusto, omnis voluptatem.
+        Laudantium, nostrum dolore quasi dolor sed minima reiciendis, amet ipsum
+        magnam odit recusandae molestias voluptatem odio. Voluptatum rem
+        consequuntur error libero quaerat sint molestias.
+      </p>
+    </section>
+  );
+};
+
+export default About;

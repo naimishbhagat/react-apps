90cf87cab53af2fc38289e0e6578083e71d7e698
naimish bhagat <naimish.bhagat@arq.group>
2020-11-19 12:34:55 +1100

    Stripe submenu added

---
+++ stripe-submenu/src/Sidebar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobalContext } from "./context";
+
+const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={`sidebar-wrapper ${isSidebarOpen ? "show" : ""}`}>
+      <div className="sidebar">
+        <buttoon className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </buttoon>
+        <div className="sidebar-links">
+          {sublinks.map((item, index) => {
+            const { links, page } = item;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((item, index) => {
+                    const { url, icon, label } = item;
+                    return (
+                      <a key={index} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
